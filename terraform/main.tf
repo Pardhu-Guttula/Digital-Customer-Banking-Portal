323443e28ed8ae84a0fa070ffa037610515cb500
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 05:20:26 +0530

    arch-gen

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,99 +1,74 @@
 provider "azurerm" {
   features {}
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.56.0"
-    }
-  }
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
 
-resource "azurerm_app_service_plan" "asp" {
-  name                = var.app_service_plan_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   sku {
     tier = "Standard"
     size = "S1"
   }
 }
 
-resource "azurerm_app_service" "app" {
-  name                = var.app_service_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+resource "azurerm_app_service" "frontend" {
+  name                = "example-frontend"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
 }
 
-resource "azurerm_api_management" "apim" {
-  name                = var.api_management_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  publisher_name      = var.publisher_name
-  publisher_email     = var.publisher_email
-  sku_name            = "Developer_1"
-}
-
-resource "azurerm_sql_server" "sql_server" {
-  name                         = var.sql_server_name
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
+resource "azurerm_sql_server" "example" {
+  name                         = "example-sqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
   version                      = "12.0"
-  administrator_login          = var.sql_admin_login
+  administrator_login          = "sqladmin"
   administrator_login_password = var.sql_admin_password
 }
 
-resource "azurerm_sql_database" "sql_db" {
-  name                = var.sql_database_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  server_name         = azurerm_sql_server.sql_server.name
-  sku_name            = "S1"
+resource "azurerm_sql_database" "example" {
+  name                = "example-sqldb"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
+  edition             = "Basic"
+  requested_service_objective_name = "Basic"
 }
 
-resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_active_directory_domain_service" "aad" {
-  domain_name          = var.aad_domain_name
-  location             = azurerm_resource_group.rg.location
-  resource_group_name  = azurerm_resource_group.rg.name
-  sku                  = "Standard"
-  decrypted_primary_password = var.aad_domain_password
-}
-
-resource "azurerm_key_vault" "kv" {
-  name                = var.key_vault_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_key_vault" "example" {
+  name                = "example-keyvault"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
 }
 
-resource "azurerm_key_vault_access_policy" "example" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
+resource "azurerm_monitor_diagnostic_setting" "example" {
+  name                        = "example-diagnosticsetting"
+  target_resource_id          = azurerm_app_service.frontend.id
+  log_analytics_workspace_id  = data.azurerm_log_analytics_workspace.example.id
+  enabled_log {
+    category    = "AppServiceHTTPLogs"
+    enabled     = true
+    retention_policy_days = null
+  }
+  metric {
+    category    = "AllMetrics"
+    enabled     = true
+    retention_policy_days = null
+  }
+}
 
-  key_permissions = [
-    "Get",
-    "List",
-  ]
-
-  secret_permissions = [
-    "Get",
-    "Set",
-  ]
+data "azurerm_log_analytics_workspace" "example" {
+  name                = "example-workspace"
+  resource_group_name = azurerm_resource_group.example.name
 }
 
 data "azurerm_client_config" "current" {}
