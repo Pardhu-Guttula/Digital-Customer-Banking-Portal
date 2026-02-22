34c7d84548cccf13679a382d0955b7e7a71baf20
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 04:40:43 +0530

    arch-gen

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,4 @@
-provider "azurerm" {
-  features {}
+terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -8,85 +7,66 @@ provider "azurerm" {
   }
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-self-service-portal"
-  location = "East US"
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_app_service_plan" "app_service_plan" {
-  name                = "appservice-plan"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+resource "azurerm_active_directory_domain_service" "example" {
+  name                = var.ad_name
+  domain_name         = var.ad_domain_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
 }
 
-resource "azurerm_app_service" "web_app" {
-  name                = "self-service-web-app"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
-  site_config {
-    always_on                = true
-    managed_pipeline_mode    = "Integrated"
-    ftps_state               = "FtpsOnly"
-  }
+resource "azurerm_b2c_directory" "example" {
+  name                = var.b2c_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
 }
 
-resource "azurerm_storage_account" "storage" {
-  name                     = "storageselfservice"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_app_service" "frontend" {
+  name                = var.app_service_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  app_service_plan_id = azurerm_app_service_plan.example.id
 }
 
-resource "azurerm_key_vault" "key_vault" {
-  name                = "selfservice-keyvault"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "standard"
+resource "azurerm_cdn_profile" "example" {
+  name                = var.cdn_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
 }
 
-resource "azurerm_log_analytics_workspace" "log_analytics" {
-  name                = "log-self-service"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "PerGB2018"
+resource "azurerm_api_management" "example" {
+  name                = var.api_management_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
 }
 
-resource "azurerm_monitor_diagnostic_setting" "diag" {
-  name                       = "diag-self-service"
-  target_resource_id         = azurerm_app_service.web_app.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
-  enabled_log {
-    category = "AppServiceHTTPLogs"
-    enabled  = true
-  }
-  metric {
-    category = "AllMetrics"
-    enabled  = true
-  }
+resource "azurerm_function_app" "example" {
+  name                = var.function_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  app_service_plan_id = azurerm_app_service_plan.example.id
+  storage_account_name = azurerm_storage_account.example.name
 }
 
-resource "azurerm_azuread_b2c_directory" "b2c" {
-  name     = "selfservice-b2c"
-  location = azurerm_resource_group.rg.location
+resource "azurerm_sql_database" "example" {
+  name                = var.sql_db_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
 }
 
-resource "azurerm_function_app" "function_app" {
-  name                       = "selfservice-functionapp"
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  app_service_plan_id        = azurerm_app_service_plan.app_service_plan.id
-  storage_account_name       = azurerm_storage_account.storage.name
-  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+resource "azurerm_cosmosdb_account" "example" {
+  name                = var.cosmosdb_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
 }
 
-resource "azurerm_logic_app" "self_service_workflow" {
-  name                = "self-service-workflow"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
 }
