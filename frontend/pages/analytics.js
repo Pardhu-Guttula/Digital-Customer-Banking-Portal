fa6083104db86bc46fe310024cc476c488bda49e
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 05:44:48 +0530

    commited by n8n

---
+++ frontend/pages/analytics.js
@@ -0,0 +1,14 @@
+// Epic Title: Develop Admin Dashboard Interface Using Next.js
+
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function Analytics() {
+  return (
+    <Layout>
+      <div>
+        <h1>Analytics & Reporting</h1>
+      </div>
+    </Layout>
+  );
+}
