15a3ec2f6eb4703c7900b7b08b52e96b26a33e5a
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 05:48:33 +0530

    commited by n8n

--- database/schema_order.sql
+++ database/schema_order.sql
@@ -1,4 +1,4 @@
-// Epic Title: Implement Backend with Node.js
+# Epic Title: Track User Orders
 
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
@@ -6,7 +6,6 @@ CREATE TABLE orders (
     address_id INT NOT NULL,
     total_amount FLOAT NOT NULL,
     status_id INT NOT NULL,
-    date TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (address_id) REFERENCES addresses(id),
     FOREIGN KEY (status_id) REFERENCES statuses(id)
