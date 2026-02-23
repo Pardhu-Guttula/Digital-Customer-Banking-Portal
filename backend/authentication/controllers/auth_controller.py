e2c4909f0f6b269c360fb27517b8fe288ff15b04
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 07:10:57 +0530

    commited by n8n

--- backend/authentication/controllers/auth_controller.py
+++ backend/authentication/controllers/auth_controller.py
@@ -1,4 +1,4 @@
-# Epic Title: Responsive Design for Tablet using React
+# Epic Title: Responsive Design for Mobile using React
 
 from flask import Blueprint, request, jsonify
 from sqlalchemy.exc import SQLAlchemyError
