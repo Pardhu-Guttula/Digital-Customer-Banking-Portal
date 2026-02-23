27b06bf335522434a47f6fd1b69cf1c09da74645
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 05:45:54 +0530

    commited by n8n

---
+++ backend/database/schema_products.sql
@@ -0,0 +1,11 @@
+# Epic Title: Store User Activity Data in PostgreSQL
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price FLOAT NOT NULL,
+    stock INT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
