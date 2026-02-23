ac15dd450e415534a4adc85180aa209f25b63e0b
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 07:09:47 +0530

    commited by n8n

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+# Epic Title: Secure Login Interface with React and MFA
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
