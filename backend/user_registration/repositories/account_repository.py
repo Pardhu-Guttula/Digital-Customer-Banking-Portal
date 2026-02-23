9a16a94ee76640adf524d4ff43e3596572b79eb8
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 07:08:06 +0530

    commited by n8n

---
+++ backend/user_registration/repositories/account_repository.py
@@ -0,0 +1,18 @@
+# Epic Title: User Registration Form
+
+from sqlalchemy.orm import Session
+from backend.user_registration.models.account import Account
+
+class AccountRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_account_by_email(self, email: str) -> Account:
+        return self.db.query(Account).filter(Account.email == email).first()
+
+    def create_account(self, name: str, email: str, password: str) -> Account:
+        account = Account(name=name, email=email, password=password)
+        self.db.add(account)
+        self.db.commit()
+        self.db.refresh(account)
+        return account
