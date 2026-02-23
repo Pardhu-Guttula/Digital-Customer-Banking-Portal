fa0fe5f61e03042060812dfd0d757dcc8fba9aec
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 05:40:49 +0530

    commited by n8n

---
+++ database/schema_login.sql
@@ -0,0 +1,4 @@
+# Epic Title: User Login
+
+-- There are no additional tables required for login, since it utilizes the users table
+-- Hence, the SQL schema remains unchanged and no new tables are required.
