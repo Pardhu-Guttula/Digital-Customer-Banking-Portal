42f3a25820e49d2f6a744264fdcad82b82bc68c1
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-23 07:09:46 +0530

    commited by n8n

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,15 @@
+# Epic Title: Secure Login Interface with React and MFA
+
+import React from 'react';
+import LoginForm from './LoginForm';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginForm />
+        </div>
+    );
+};
+
+export default App;
