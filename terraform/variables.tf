d2f3b83b8d847a734816c709a8511f129b21b170
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 02:40:07 +0530

    arch-gen

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,31 @@
-variable "sql_admin_password" {
-  description = "The password for the SQL administrator"
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "example-resources"
+}
+
+variable "location" {
+  description = "The Azure location where resources will be deployed"
+  type        = string
+  default     = "East US"
+}
+
+variable "app_service_name" {
+  description = "The name of the App Service"
+  type        = string
+}
+
+variable "static_site_name" {
+  description = "The name of the Static Site"
+  type        = string
+}
+
+variable "function_app_name" {
+  description = "The name of the Function App"
+  type        = string
+}
+
+variable "logic_app_name" {
+  description = "The name of the Logic App"
   type        = string
 }
