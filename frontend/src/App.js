10f081b5968e29969559a84712dc527915dad0aa
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 01:15:18 +0530

    commited by n8n

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+# Epic Title: Develop Responsive Design for the Portal Using React
+
+import React from 'react';
+import PortalLayout from './components/PortalLayout';
+
+function App() {
+  return (
+    <div className="App">
+      <PortalLayout />
+    </div>
+  );
+}
+
+export default App;
