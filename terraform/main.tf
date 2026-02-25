d42ed0fc5a0584c8ebc8273510c97d7ae1229adc
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 02:43:49 +0530

    arch-gen

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,45 +1,10 @@
 provider "azurerm" {
   features {}
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.56.0"
-    }
-  }
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
-}
-
-resource "azurerm_app_service" "web" {
-  name                = "example-app-service"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
-}
-
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-app-service-plan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  sku_name            = "B1"
-}
-
-resource "azurerm_static_site" "example" {
-  name                = "example-static-site"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-}
-
-resource "azurerm_function_app" "example" {
-  name                       = "example-function-app"
-  location                   = azurerm_resource_group.example.location
-  resource_group_name        = azurerm_resource_group.example.name
-  app_service_plan_id        = azurerm_app_service_plan.example.id
-  storage_account_name       = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_storage_account" "example" {
@@ -50,18 +15,81 @@ resource "azurerm_storage_account" "example" {
   account_replication_type = "LRS"
 }
 
-resource "azurerm_logic_app_workflow" "example" {
-  name                = "example-logic-app"
+resource "azurerm_static_site" "example" {
+  name                = "examplestaticwebsite"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku_tier            = "Free"
+
+  provider_b2c  = "aadB2C"
+  custom_domain = "example.com"
+}
+
+resource "azurerm_cdn_profile" "example" {
+  name                = "exampleCdnProfile"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku {
+    name = "Standard_Microsoft"
+  }
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-resources"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
 }
 
-resource "azurerm_application" "aad" {
-  name          = "example-app"
-  application_id = "00000000-0000-0000-0000-000000000000"
+resource "azurerm_app_service" "example" {
+  name                = "example-app"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
 }
 
-resource "azurerm_application" "aad_b2c" {
-  name          = "example-app-b2c"
-  application_id = "00000000-0000-0000-0000-000000000000"
+resource "azurerm_api_management" "example" {
+  name                = "exampleAPI"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  publisher_name      = "ExamplePublisher"
+  publisher_email     = "example@example.com"
+  sku_name            = "Developer_1"
+}
+
+resource "azurerm_sql_server" "example" {
+  name                = "mysqlserver"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  version             = "12.0"
+  administrator_login = "sqladmin"
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_sql_database" "example" {
+  name                = "mydatabase"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  server_name         = azurerm_sql_server.example.name
+  edition             = "Basic"
+}
+
+resource "azurerm_cosmosdb_account" "example" {
+  name                = "cosmosdbaccount"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+  consistency_policy {
+    consistency_level = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix = 200
+  }
+  geo_location {
+    location = azurerm_resource_group.example.location
+    failover_priority = 0
+  }
 }
